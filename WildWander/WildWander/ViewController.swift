eea924cff09ee5d642ed620fa05cd9b00bd77403
nuca01 <nucaberiashvili16@gmail.com>
2024-07-02 01:09:05 +0400

    create the project

---
+++ WildWander/WildWander/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WildWander
+//
+//  Created by nuca on 01.07.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
