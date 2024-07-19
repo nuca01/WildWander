447d5ecb2cfe46067772c2277ad1685e378588ba
nuca01 <nucaberiashvili16@gmail.com>
2024-07-19 23:18:24 +0400

    add profile page with a login page pop up

---
+++ WildWander/WildWander/Scenes/Profile Page/ProfilePageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProfilePageViewController.swift
+//  WildWander
+//
+//  Created by nuca on 19.07.24.
+//
+
+import UIKit
+
+class ProfilePageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
