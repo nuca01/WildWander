63793878ea26b2c4290a7cdc105b6beb0cbbe37e
nuca01 <nucaberiashvili16@gmail.com>
2024-07-22 02:37:19 +0400

    add saving logic to explore page, make SearchPage more user-friendly

---
+++ WildWander/WildWander/Scenes/Saved Page/Lists/CreateAListView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateAListView.swift
+//  WildWander
+//
+//  Created by nuca on 21.07.24.
+//
+
+import SwiftUI
+
+struct CreateAListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateAListView()
+}
