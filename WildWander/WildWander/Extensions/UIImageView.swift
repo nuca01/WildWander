3caba5be993f8c3cd67dcafc876186b4c194d01c
nuca01 <nucaberiashvili16@gmail.com>
2024-07-23 03:41:53 +0400

    add loader to photos that are fetched asynchronously

--- WildWander/WildWander/Extensions/UIImageView.swift
+++ WildWander/WildWander/Extensions/UIImageView.swift
@@ -6,17 +6,35 @@
 //
 
 import UIKit
+import SwiftUI
 
 extension UIImageView {
     func load(url: URL) {
+        let loaderView = addLoader()
         DispatchQueue.global().async { [weak self] in
             if let data = try? Data(contentsOf: url) {
                 if let image = UIImage(data: data) {
                     DispatchQueue.main.async {
+                        loaderView?.removeFromSuperview()
                         self?.image = image
                     }
                 }
             }
         }
+    }
+    
+    func addLoader() -> UIView? {
+        let loaderView = UIHostingController(rootView: LoaderView()).view
+        loaderView?.translatesAutoresizingMaskIntoConstraints = false
+        if let loaderView {
+            addSubview(loaderView)
+            NSLayoutConstraint.activate([
+                loaderView.heightAnchor.constraint(equalToConstant: 20),
+                loaderView.widthAnchor.constraint(equalToConstant: 20),
+                loaderView.centerYAnchor.constraint(equalTo: centerYAnchor),
+                loaderView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            ])
+        }
+        return loaderView
     }
 }
